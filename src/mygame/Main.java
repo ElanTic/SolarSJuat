8bed3777b4d02529ab07d34511887877623a4df0
ElanTic <aplicacionesdejon@gmail.com>
2024-03-04 16:10:20 -0600

    agregado metodo create planet geometry

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -6,7 +6,6 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
 import com.jme3.scene.Spatial;
@@ -35,48 +34,54 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Celestial sol = new Celestial("sol");
+        Celestial sol =    new CelestialBuilder("sol")
+                .orbita(new Orbita(0, 1, 0))
+                .build();
+
+        Celestial tierra = new CelestialBuilder("tierra")
+                .body(createPlanetGeometry(ColorRGBA.Blue)) 
+                .rotacion(new Rotacion(0, 5, 0))
+                .orbita(new Orbita(0, 2, 0))
+                .parent(sol) 
+                .build();
+
+        Celestial luna = new CelestialBuilder("luna")
+                .body(createLunaGeometry(ColorRGBA.Red)) 
+                .rotacion(new Rotacion(0, 2, 0))
+                .parent(tierra) 
+                .build();
         
-        Celestial tierra = new Celestial ("tierra");
-        Sphere planetMesh = new Sphere(32, 32, 1); // Adjust segments and radius as needed
+        tierra.move(4, 0, 0);
+        luna.move(5, 2, 2);
+
+        rootNode.attachChild(sol);
+    }
+    
+    private Spatial createPlanetGeometry(ColorRGBA color) {
+        Sphere planetMesh = new Sphere(32, 32, 1); 
         Geometry planetGeometry = new Geometry("Planet", planetMesh);
         Material planetMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        planetMaterial.setColor("Color", ColorRGBA.Blue); // Adjust color as needed
+        planetMaterial.setColor("Color", color); 
+        planetMaterial.setTexture("ColorMap", assetManager.loadTexture("Textures/pixelated_image.png"));
         planetGeometry.setMaterial(planetMaterial);
-        
-        Celestial luna = new Celestial("luna");
+        return planetGeometry;
+    }
+
+    private Spatial createLunaGeometry(ColorRGBA color) {
         Box b = new Box(.2f, .2f, .2f);
         Geometry lunaGeom = new Geometry("lunaGeom", b);
-        
-        Box b2 = new Box(1, 1, 1);
-        Geometry tierraGeom = new Geometry ("tierraGeom", b2);
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Red);
+        mat.setColor("Color", color);
         mat.setTexture("ColorMap", assetManager.loadTexture("Textures/pixelated_image.png"));
         lunaGeom.setMaterial(mat);
         lunaGeom.rotate(0, 0, FastMath.HALF_PI);
-        
-        tierraGeom.setMaterial(mat);
-        tierraGeom.rotate(0,0, FastMath.HALF_PI/2);
-                
-        sol.setOrbita(new Orbita(0,1,0));
-        sol.attachChild(tierra);
-        tierra.setBody(tierraGeom);
-        luna.setBody(lunaGeom);
-        luna.setRotacion(new Rotacion(0,2,0));
-        tierra.attachChild(luna);
-        tierra.setRotacion(new Rotacion(0,5,0));
-        tierra.setOrbita(new Orbita(0,2,0));
-        tierra.move(4,0,0);
-        luna.move(5,2,2);
-        rootNode.attachChild(sol);
-        tierra.setBody(planetGeometry);
+
+        return lunaGeom;
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
         SistemaOrbita.update(tpf, (Celestial) rootNode.getChild("sol"));
         SistemaRotacion.update(tpf, (Celestial)rootNode.getChild("sol"));  
     }
